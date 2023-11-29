b3431251ffcc9d9400de86b75f2ae534f6206c7d
davitpra <davitpra@Davit>
2023-11-29 12:03:03 -0500

    lab 7 database

--- Database1/lab 7/process-form.php
+++ Database1/lab 7/process-form.php
@@ -1,27 +1,39 @@
 <?php
 
-$fname = $_POST["fname"];
-$lname = $_POST["lname"];
+$fname = $_POST["first_name"];
+$lname = $_POST["last_name"];
+$password = $_POST['password'];
 $email = $_POST["email"];
 $address = $_POST["address"];
 $city = $_POST["city"];
 $country = $_POST["country"];
 
-print_r($_POST);
-
-
-// if ( ! $terms) {
-//     die("Terms must be accepted");
-// }   
-
-// $host = "localhost";
-// $dbname = "message_db";
-// $username = "root";
-// $password = "";
+$host = "127.0.0.1:3307";
+$dbname = "lab8";
+$username = "root";
+$dbpassword = "";
         
-// $conn = mysqli_connect(hostname: $host,
-//                        username: $username,
-//                        password: $password,
-//                        database: $dbname);
+$mysqli = mysqli_connect(hostname: $host,
+                         username: $username,
+                         password: $dbpassword,
+                         database: $dbname);
 
-// echo "Record saved.";
+if (mysqli_connect_errno()) {
+    die("Connection error: " . mysqli_connect_error());
+}           
+
+$sql = "INSERT INTO registrations (first_name, last_name, password, email, address, city, country) 
+        VALUES ('$fname', '$lname', '$password', '$email', '$address', '$city', '$country')";
+
+
+if (isset($_POST['submit'])) {
+    $result = mysqli_query($mysqli, $sql);
+
+    if ($result) {
+        echo "Record saved.";
+    } else {
+        echo "Error at saving: " . mysqli_error($mysqli);
+    }
+} else {
+    echo "Form not submitted.";
+}
